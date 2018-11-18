3eef1d08c760b7693620db40608caccdc41e6c48
davidm15 <david.mischak@edu.fh-joanneum.at>
2018-11-18 19:26:44 +0100

    init

---
+++ Assets/scripts/demo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class demo : MonoBehaviour {
+
+	public float x;
+	public float y;
+	public float z;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Vector3 go = new Vector3 (x, y, z);
+
+		transform.Rotate (go);
+	}
+}
